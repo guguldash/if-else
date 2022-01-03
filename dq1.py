a3dec5735e7f25c09da7ac7a32f06872a1aff83d
guguldash <84378775+guguldash@users.noreply.github.com>
2022-01-03 11:41:59 +0530

    Add files via upload

---
+++ dq1.py
@@ -0,0 +1,9 @@
+num1=int(input("enter your number"))
+num2=int(input("enter your numer"))
+if num1>=num2:
+    print("num1 is greater")
+elif num1<=num2:
+    print("num2 is greater")
+else:
+    print("both are equal")
+
