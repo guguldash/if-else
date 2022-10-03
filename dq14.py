aea76b52ab43838f6331e219fc297afd79a1760e
Sourcery AI <>
2022-10-03 04:46:07 +0000

    'Refactored by Sourcery'

--- dq14.py
+++ dq14.py
@@ -1,9 +1,7 @@
 a = int(input('Please Enter the First Angle of a Triangle: '))
 b = int(input('Please Enter the Second Angle of a Triangle: '))
 c = int(input('Please Enter the Third Angle of a Triangle: '))
-if a+b>c:
+if a + b > c or b + c > a:
     print("valid")
-elif b+c>a:
-    print("valid") 
 else:
     print ("not valid")        
